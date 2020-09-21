1e757785a5d14cd2740916a1c7f7ecc1175ca84c
Erik Ralston <erikralston310@gmail.com>
2020-09-21 15:39:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+import express from 'express';
+const app = express();
+
+// app.get creates a Route Handler. '/' is the "Route"
+app.get('/', (req, res) => {
+  res.send({
+    hi: 'there'
+  });
+});
+
+// set up dynamic port binding and tell Node which port to listen to. 
+const PORT = process.env.PORT || 5000; // 5000 is the backup default for dev
+app.listen(PORT);
